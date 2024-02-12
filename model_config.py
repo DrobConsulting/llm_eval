334b514622175f8aa69c2e49e21c1a2636bd2b72
Michael Drob <drob7315@gmail.com>
2024-02-12 00:11:09 -0500

    fixed prompts

--- model_config.py
+++ model_config.py
@@ -3,10 +3,10 @@ import json
 MODELS_TOP1 = [
   {
     "name": "custom-model",
-    "displayName": "Code Llama 13b Instruct",
+    "displayName": "Code Llama Instruct",
     "description": "This is a custom model configured to work with my local server.",
-    "preprompt": "Use [PYTHON] [/PYTHON]: ",
-    "chatPromptTemplate": "[INST] {{content}} [/INST]",
+    "preprompt": "",
+    "chatPromptTemplate": '[INST] You are an expert Python programmer, and here is your task: {task}\nYour code should pass these tests:\n\n{tests}\nYour code should start with a [PYTHON] tag and end with a [/PYTHON] tag.\n [/INST]',
     "parameters": {
       "temperature": 0.001,
       "top_p": 0.95,
@@ -14,7 +14,7 @@ MODELS_TOP1 = [
       "top_k": 100,
       "truncate": 512,
       "do_sample": False,
-      "max_new_tokens": 1024,  # Adjusted to match your server setup
+      "max_new_tokens": 512,  # Adjusted to match your server setup
       "stop_sequences": ["</s>"]
     },
     "promptExamples": [
@@ -30,18 +30,18 @@ MODELS_TOP1 = [
 MODELS_TOPK = [
   {
     "name": "custom-model",
-    "displayName": "Code Llama 13b Instruct",
+    "displayName": "Code Llama Instruct",
     "description": "This is a custom model configured to work with my local server.",
-    "preprompt": "Use [PYTHON] [/PYTHON]: ",
-    "chatPromptTemplate": "[INST] {{content}} [/INST]",
+    "preprompt": "",
+    "chatPromptTemplate": '[INST] You are an expert Python programmer, and here is your task: {task}\nYour code should pass these tests:\n\n{tests}\nYour code should start with a [PYTHON] tag and end with a [/PYTHON] tag.\n [/INST]',
     "parameters": {
       "temperature": 0.8,
       "top_p": 0.95,
       "repetition_penalty": 1.2,
-      "top_k": 50,
-      "truncate": 1024,
+      "top_k": 100,
+      "truncate": 512,
       "do_sample": True,
-      "max_new_tokens": 1024,  # Adjusted to match your server setup
+      "max_new_tokens": 512,  # Adjusted to match your server setup
       "stop_sequences": ["</s>"]
     },
     "promptExamples": [
